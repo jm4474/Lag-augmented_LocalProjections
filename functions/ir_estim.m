4b968289f7df18bf5dd38b0d0f0cae508351eca5
mikkelpm <mikkel.p.m@gmail.com>
2020-06-24 19:15:51 -0400

    Simpler way of specifying optional arguments in "ir_estim" + gitignore

--- functions/ir_estim.m
+++ functions/ir_estim.m
@@ -1,13 +1,9 @@
-function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings)
+function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, p, horzs, varargin)
 
     % Wrapper function for AR or LP estimation of impulse responses
     % Delta method and bootstrap confidence intervals
     
-    % Inputs:
-    % Y         T x 1   data vector
-    % spec      struct  estimator specification
-    % settings  struct  estimation settings
-    % varargin          OPTIONAL: estimator specification used to generate AR bootstrap samples
+    % Inputs: see below
     
     % Outputs:
     % irs       H x 1       estimated impulse responses
@@ -18,35 +14,61 @@ function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings
     % res                   residuals in regression(s)
     
     
+    %% Parse optional inputs
+    
+    ip = inputParser;
+    
+    % Required inputs
+    addRequired(ip, 'Y', @isnumeric);       % T x 1     data vector
+    addRequired(ip, 'p', @isnumeric);       % 1 x 1     lag length (not counting any augmentation)
+    addRequired(ip, 'horzs', @isnumeric);   % 1 x m     impulse response horizons of interest
+    
+    % Optional inputs
+    addParameter(ip, 'alpha', 0.05, @isnumeric); % Significance level (default: 0.05)
+    addParameter(ip, 'estimator', 'lp', @ischar); % Estimator type, either 'ar' or 'lp' (default: local projection)
+    addParameter(ip, 'lag_aug', false, @islogical); % Lag-augment? (default: no)
+    addParameter(ip, 'bias_corr', true, @islogical); % Bias-correct AR estimates? (default: yes)
+    addParameter(ip, 'se_homosk', false, @islogical); % Homoskedastic standard errors/bootstrap? (default: no)
+    addParameter(ip, 'no_const', false, @islogical); % Omit intercept? (default: no)
+    addParameter(ip, 'har', [], @(x) isa(x, 'function_handle') || isempty(x)); % HAR estimator as function of data and bandwidth, empty if no HAR (default: no HAR)
+    addParameter(ip, 'har_bw', [], @(x) isa(x, 'function_handle') || isempty(x)); % HAR bandwidth as function of sample size
+    addParameter(ip, 'har_cv', [], @(x) isa(x, 'function_handle') || isempty(x)); % HAR critical value as function of bandwidth, empty if normal critical value
+    addParameter(ip, 'bootstrap', [], @(x) ischar(x) || isempty(x)); % Bootstrap type, either 'ar', 'resid', or 'pair'; empty if delta method (default: delta method)
+    addParameter(ip, 'boot_num', 1000, @isnumeric); % Bootstrap iterations (default: 1000)
+    addParameter(ip, 'boot_lag_aug', false, @islogical); % Lag-augment in bootstrap DGP? (default: no)
+    
+    parse(ip, Y, p, horzs, varargin{:});
+    
+    
     %% Preliminaries
     
     T = length(Y);              % Sample size
     
     numhorz ...
-      = length(settings.horzs); % Number of horizons
+      = length(horzs); % Number of horizons
     
     cvs ...
-      = repmat(norminv(1-settings.alpha/2),...
+      = repmat(norminv(1-ip.Results.alpha/2),...
               1, numhorz);      % Default critical values: normal
           
     cis_boot ...
-      = nan(2,numhorz,3);       % Initializes NaN array.
+      = nan(2,numhorz,3);       % Initializes NaN array
     
     
     %% Point estimates and s.e.
     
-    if strcmp(spec.estimator, 'ar') % AR
+    if strcmp(ip.Results.estimator, 'ar') % AR
         
         % AR impulse responses
         [irs, ses, ~, betahat, res] = ar_ir_estim(Y, ...
-                                                  settings.p,...
-                                                  settings.p+spec.lag_aug,...
-                                                  settings.horzs, ...
-                                                  spec.biascorr,...
-                                                  settings.se_homosk,...
-                                                  settings.noconst);
+                                                  p,...
+                                                  p+ip.Results.lag_aug,...
+                                                  horzs, ...
+                                                  ip.Results.bias_corr,...
+                                                  ip.Results.se_homosk,...
+                                                  ip.Results.no_const);
         
-    elseif strcmp(spec.estimator, 'lp') % LP
+    elseif strcmp(ip.Results.estimator, 'lp') % LP
         
         irs = zeros(numhorz,1);
         
@@ -61,40 +83,45 @@ function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings
         
         for h=1:numhorz % For each horizon...
             
-            the_horz = settings.horzs(h); % Horizon
+            the_horz = horzs(h); % Horizon
             
             % Determine s.e. setting and c.v.
             
-            if spec.har % HAR
+            if ~isempty(ip.Results.har) % HAR
                 the_bw ...
-                    = settings.har_bw(T-settings.p-spec.lag_aug-the_horz); 
+                    = ip.Results.har_bw(T-p-ip.Results.lag_aug-the_horz); 
                     % Bandwidth, determined by effective sample size
                     
                 the_se_setting ...
-                    = @(X) settings.har_fct(X,the_bw); 
+                    = @(X) ip.Results.har(X,the_bw); 
                     % HAR function
-                    
-                cvs(h) ...
-                    = settings.har_cv(the_bw); % Critical value
+                 
+                if ~isempty(ip.Results.har_cv)
+                    cvs(h) ...
+                        = ip.Results.har_cv(the_bw); % Critical value
+                end
                 
             else % EHW/homoskedastic
                 
-                the_se_setting = settings.se_homosk; % Indicator for whether homosk. or EHW
+                the_se_setting = ip.Results.se_homosk; % Indicator for whether homosk. or EHW
             
             end
             
             % LP regression
-            %settings p is the dimension of the AR model
-            %spec.lag_aug is whether or not the LP is lag augmented 
             [irs(h), ses(h), betahat{h},...
             ~, res{h}, X{h}] = lp(Y, ...
-                                  settings.p-1+spec.lag_aug,...                                                                                          
+                                  p-1+ip.Results.lag_aug,...                                                                                          
                                   the_horz,...                  
                                   the_se_setting,...
-                                  settings.noconst);
+                                  ip.Results.no_const);
             
         end
         
+    end
+    
+    % If only point estimates and standard errors are requested, stop
+    if nargout<=2
+        return;
     end
     
     
@@ -105,46 +132,32 @@ function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings
     
     %% Bootstrap confidence intervals
     
-    if isfield(spec, 'bootstrap') && ~isempty(spec.bootstrap)
+    if ~isempty(ip.Results.bootstrap)
         
-        estims_boot = zeros(settings.numboot,numhorz);
+        estims_boot = zeros(ip.Results.boot_num,numhorz);
         ses_boot = estims_boot;
         
-        if strcmp(spec.bootstrap, 'ar') % Recursive AR bootstrap specifications
-            
-            spec_boot_ar = spec.bootstrap_spec; 
-                         % Specification for generating bootstrap samples
-                         
-            spec_boot_ar.bootstrap ...
-                         = []; % Don't do another bootstrap when estimating bootstrap DGP
+        if strcmp(ip.Results.bootstrap, 'ar') % Recursive AR bootstrap specifications
             
             % AR coefficient estimates that define bootstrap DGP
             
-            %[~, ~, ~, ~, betahat_ar, res_ar] ...
-            %            = ir_estim(Y, spec_boot_ar, settings);
             [~, ~, ~, betahat_ar, res_ar] = ar_ir_estim(Y, ...
-                                                  settings.p,...
-                                                  settings.p+spec_boot_ar.lag_aug,...
-                                                  settings.horzs, ...
-                                                  spec_boot_ar.biascorr,...
-                                                  settings.se_homosk,...
-                                                  settings.noconst);
-                                              
-            if isfield(settings, 'boot_ar_restrict') && ~isempty(settings.boot_ar_restrict)
-                
-                betahat_ar(1:settings.p) = settings.boot_ar_restrict(betahat_ar(1:settings.p)); % Restrict coefficient estimates to stationary region
+                                                  p,...
+                                                  p+ip.Results.boot_lag_aug,...
+                                                  horzs, ...
+                                                  ip.Results.bias_corr,...
+                                                  ip.Results.se_homosk,...
+                                                  ip.Results.no_const);
             
-            end
-            
-            pseudo_truth = ar_ir(betahat_ar(1:settings.p), settings.horzs); % Pseudo-true impulse responses in bootstrap DGP
+            pseudo_truth = ar_ir(betahat_ar(1:p), horzs); % Pseudo-true impulse responses in bootstrap DGP
     
-            for b=1:settings.numboot
+            for b=1:ip.Results.boot_num
 
                 % Generate bootstrap sample based on (possibly lag-augmented) AR estimates
-                Y_boot = ar_boot(betahat_ar, res_ar, Y, settings.p+spec_boot_ar.lag_aug, settings.se_homosk, settings.noconst);
+                Y_boot = ar_boot(betahat_ar, res_ar, Y, p+ip.Results.boot_lag_aug, ip.Results.se_homosk, ip.Results.no_const);
 
                 % Estimate on bootstrap sample
-                [estims_boot(b,:),ses_boot(b,:)] = ir_estim(Y_boot, rmfield(spec, 'bootstrap'), settings);
+                [estims_boot(b,:),ses_boot(b,:)] = ir_estim(Y_boot, p, horzs, varargin{:});
 
             end
 
@@ -154,13 +167,13 @@ function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings
   
             for h=1:numhorz % Treat each horizon separately
 
-                for b=1:settings.numboot
+                for b=1:ip.Results.boot_num
 
                     % Generate bootstrap sample
-                    [Y_boot, X_boot] = linreg_boot(betahat{h},res{h},X{h},spec.bootstrap,settings.se_homosk);
+                    [Y_boot, X_boot] = linreg_boot(betahat{h},res{h},X{h},ip.Results.bootstrap,ip.Results.se_homosk);
 
                     % Run OLS on bootstrap sample
-                    [the_linreg_betahat, the_linreg_ses] = linreg(Y_boot,X_boot,settings.se_homosk,true); % Don't add extra intercept
+                    [the_linreg_betahat, the_linreg_ses] = linreg(Y_boot,X_boot,ip.Results.se_homosk,true); % Don't add extra intercept
                     estims_boot(b,h) = the_linreg_betahat(1);
                     ses_boot(b,h) = the_linreg_ses(1);
 
@@ -171,7 +184,7 @@ function [irs, ses, cis_dm, cis_boot, betahat, res] = ir_estim(Y, spec, settings
         end
         
         % Compute bootstrap confidence intervals
-        cis_boot = boot_ci(pseudo_truth', irs', ses', estims_boot, ses_boot, settings.alpha);
+        cis_boot = boot_ci(pseudo_truth', irs', ses', estims_boot, ses_boot, ip.Results.alpha);
         
     end
 
